be7e9097e47733a51cace6cfaa5ef0e5879c7b12
Marko Budisic <mbudisic@gmail.com>
2015-09-10 10:59:05 -0500

    Made de-biasing optional

--- +koopman/DMD.m
+++ +koopman/DMD.m
@@ -1,16 +1,24 @@
-function [lambda, Modes] = DMD(Snapshots, dt, Nmd)
+function [lambda, Modes] = DMD(Snapshots, dt, Nmd, debias)
+%DMD Compute Dynamic Mode Decomposition using exact DMD
+
   % We assume that OutputSnapshots = KoopmanOperator( InputSnapshots )
   % column-by-column
   InputSnapshots = Snapshots(:,1:end-1);
   OutputSnapshots = Snapshots(:,2:end);
 
-  % %% De-bias input Data (Hemati, Rowley)
-  % r = 20;
-  % Augmented = [InputSnapshots; OutputSnapshots];
-  % [Un, Sn, Vn] = svd(Augmented);
-  % P = Vn(:,1:r) * Vn(:,1:r)';
-  % InputSnapshots = InputSnapshots * P;
-  % OutputSnapshots = OutputSnapshots * P;
+  %% De-bias input Data (Hemati, Rowley)
+  if nargin == 4 && debias
+    if islogical(debias)
+      r = 2*Nmd;
+    else
+      r = debias;
+    end
+    Augmented = [InputSnapshots; OutputSnapshots];
+    [Un, Sn, Vn] = svd(Augmented);
+    P = Vn(:,1:r) * Vn(:,1:r)';
+    InputSnapshots = InputSnapshots * P;
+    OutputSnapshots = OutputSnapshots * P;
+  end
 
   %% "Exact" DMD
   [Ux, Sx, Vx] = svd( Snapshots(:,1:end-1), 'econ' );
