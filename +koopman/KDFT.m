1cafbd109e27bccc834a70dd98dfbd8920ae2fc6
Marko Budisic <mbudisic@gmail.com>
2015-09-11 13:52:07 -0500

    Documentation for KDFT

--- +koopman/KDFT.m
+++ +koopman/KDFT.m
@@ -1,6 +1,24 @@
 function [lambda, Modes] = KDFT(Snapshots, dt)
 %KDFT Compute Koopman modes using Discrete Fourier Transform (FFT).
 %
+% [lambda, Modes] = KDFT( Snapshots, dt )
+%    Compute Koopman modes of data in Snapshots matrix. Columns of Snapshots are
+%    measurements taken dt apart.
+%
+%    Koopman modes are taken by computing FFT of every *row* of Snapshots
+%    matrix, and then sorting those frequencies that give contribution to a
+%    large number of observables.
+%
+%    lambda -- list of complex Koopman frequencies, real part is the
+%    decay rate, imaginary part (angular) frequency.
+%    Modes  -- each column of the matrix is a Koopman mode, corresponding
+%    to the lambda at the same index.
+%
+%    lambda and Modes are sorted by l2-norm of columns of Modes, in
+%    descending order.
+
+
+% Copyright information in LICENSE file of the package.
 
 % number of snapshots
 N = size(Snapshots, 2);
