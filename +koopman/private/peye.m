a2705c60523e3ca6edae4ce4e1e093a49fe9f133
Marko Budisic <mbudisic@gmail.com>
2015-09-11 11:19:10 -0500

    peye now works similar to MATlAB's pinv

--- +koopman/private/peye.m
+++ +koopman/private/peye.m
@@ -1,11 +1,35 @@
-function X = peye(A)
-  [U,S,V] = svd(A,'econ');
-  s = diag(S);
-  if nargin < 2
-    tol = max(size(A)) * eps(norm(s,inf));
-  end
-  V = V(:,s>tol);
-  X = V*V';
+function X = peye(A,r)
+%PEYE Compute projector onto the input space of matrix A.
+%
+%  PEYE(A) If A is non-singular, result is the identity matrix. Otherwise, it
+%  is the projector V*V' where A = USV is the economy SVD of V, where only
+%  dominant singular values are retained.
+%
+%  PEYE(..., r) As above, except exactly r dominant singular vectors are retained.
+%
 
+  [~,S,V] = svd(A,'econ');
+  s = diag(S);
+
+  if nargin < 2
+    r = nan;
+  else
+    try
+      validateattributes(r,{'logical'},{'scalar'});
+      r = nan;
+    catch
+      validateattributes(r,{'numeric'},...
+                         {'positive','finite','integer'});
+    end
+  end
+
+  if isnan(r)
+    tol = max(size(A)) * eps(norm(s,inf));
+    r = sum(s > tol)+1;
+  end
+
+  V(:,r:end) = [];
+  s = 1./s(:);
+  X = V*V';
 
 end
