b4f957147d346f599a279749e0f52042f4aed600
Marko Budisic <mbudisic@gmail.com>
2015-09-11 14:05:22 -0500

    Contents.m in +koopman added

---
+++ +koopman/Contents.m
@@ -0,0 +1,8 @@
+% KOOPMAN
+%
+% Files
+%   DMD        - Compute Koopman modes by "exact" Dynamic Mode Decomposition of Tu et al.
+%   DMD_Duke   - Compute Koopman modes by Dynamic Mode Decomposition algorithm by Duke et al.
+%   KDFT       - Compute Koopman modes using Discrete Fourier Transform (FFT).
+%   NDMD       - Compute DMD for nonuniformly sampled data.
+%   removemean - Removes time mean from snapshots.
