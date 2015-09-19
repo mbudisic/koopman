75935012bb40484ef94ff7700a454384326a62b0
Marko Budisic <mbudisic@gmail.com>
2015-09-18 23:07:34 -0500

    Added VonKarmanStreet example

--- examples/VonKarmanStreet.m
+++ examples/VonKarmanStreet.m
@@ -1,21 +1,29 @@
+%% KOOPMAN DECOMPOSITION OF VON KARMAN STREET
+% This is a demo of Koopman decomposition of a fluid flow data.
+%%
+
+% Copyright 2015 under BSD license (see LICENSE file).
+
 function VonKarmanStreet( recompute )
-% # Koopman decomposition of Von Karman street
 
-% # Load the velocity field
-disp('Loading data.')
+%% Load the velocity field
+assert(exist('VonKarmanStreet.mat','file'),...
+       'Download data file from <a href="https://dl.dropboxusercontent.com/u/14017882/data/VonKarmanStreet.mat">https://dl.dropboxusercontent.com/u/14017882/data/VonKarmanStreet.mat</a>')
 load VonKarmanStreet.mat
+disp(DESCRIPTION)
+disp(README)
 
-% ##
-% # We will use speed and vorticity as observables.
-disp('Computing Speed and Vorticity')
+%% Compute speed and vorticity
+% These quantities will be used as measurements
 
-% compute discrete derivatives and pad the matrices to recover the correct
-% dimensions
-Uy = diff(U, 1, 2 );
-Uy(:,end+1,:,:) = Uy(:,end,:,:);
-
+%
+% Evaluate discrete derivatives used to compute vorticity
 Ux = diff(U, 1, 3 );
+Uy = diff(U, 1, 2 );
+
+% Pad to preserve data size
 Ux(:,:,end+1,:) = Ux(:,:,end,:);
+Uy(:,end+1,:,:) = Uy(:,end,:,:);
 
 % vorticity
 W = Ux(2,:,:,:) - Uy(1,:,:,:);
@@ -23,105 +31,124 @@ W = Ux(2,:,:,:) - Uy(1,:,:,:);
 % speed
 V = sqrt(sum(U.^2,1));
 
-%%
-% Concatenate data
+% stack data
 Data = cat(1, V, W );
+[nVars, nRows, nCols, nSteps] = size(Data);
+% Data is a 4-d array
+
+% plot the first step
+plotData(Data, 1)
+
+%% Format data to suit Koopman toolbox
+
+% Each column in Snapshots contains all the data recorded at a single timestep
+Snapshots = reshape(Data, nVars*nRows*nCols, nSteps );
+% Snapshots is now a 2d array (matrix)
 
 %%
-% Convert to Snapshots format -- stack all data at a single time step into
-% its own column
-nVars = size(Data,1);
-nRows = size(Data,2);
-nCols = size(Data,3);
-nSteps=  size(Data,4);
-
-disp('Computing Snapshots matrix');
-Snapshots = reshape(Data, nVars*nRows*nCols, nSteps );
-
-disp('Remove the mean')
+% Remove the mean
 Snapshots = koopman.removemean(Snapshots);
 
-%%
-% Compute Koopman modes
+%% Compute Koopman modes
+
+% Use persistent variables to avoid recomputing Modes,
+% e.g., when just re-plotting data
 persistent Spectrum
 persistent Modes
 persistent Amps
 
+% If no modes have been computed, or recomputation is explictly requested,
+% compute the Koopman modes
 if isempty(Spectrum) || (nargin >= 1 && recompute)
-  disp('Computing Koopman modes');
+
+  %%
+  % Main invocation of the computation
   [Spectrum, Modes, Amps] = koopman.DMD( Snapshots, 1, true);
-  % Reshape modes back into Data-style format
+
+  %%
+
+  % Reshape modes back into Data-style 4-d format
   nModes = numel(Spectrum);
   Modes = real(reshape(Modes, [nVars,nRows,nCols,nModes]));
-else
-  disp('Reusing already computed Koopman modes')
 end
 
-%%
-% Plot the first frame of data
-disp('Plotting data');
-figure('Name','Data');
-subplot(2,1,1);
-pcolor(squeeze(Data(1,:,:,1)))
-shading interp
-xlabel('X');
-ylabel('Y');
-title('Speed')
-axis equal tight
-set(gca,'XTick',[],'YTick',[])
-
-subplot(2,1,2);
-pcolor(squeeze(Data(2,:,:,1)))
-shading interp
-xlabel('X');
-ylabel('Y');
-title('Vorticity')
-axis equal tight
-set(gca,'XTick',[],'YTick',[])
-
-%%
-% Plot the spectrum
-disp('Plotting the spectrum');
+%% Plot the spectrum
 figure('Name','Spectrum')
 koopman.plotSpectrum( Spectrum, Amps )
 
 %%
-% Plotting the modes
+% The size of the marker indicates the L2-optimal magnitude of the mode, and
+% its color indicates the L2-optimal phase of the mode
+
+%% Plot the modes
 figure('Name','Modes');
 plotModes(4, Spectrum, Amps, Modes)
 
-end
+%% Auxiliary plotting functions
+
+%%% PLOTDATA
+% Plot one step in data matrix
+function plotData( Data, step )
+
+figure('Name','Data');
+subplot(2,1,1);
+pcolor(squeeze(Data(1,:,:,step)))
+shading interp
+xlabel('X');
+ylabel('Y');
+title(sprintf('Speed at step %d',step) )
+axis equal tight
+set(gca,'XTick',[],'YTick',[])
+
+subplot(2,1,2);
+pcolor(squeeze(Data(2,:,:,step)))
+shading interp
+xlabel('X');
+ylabel('Y');
+title(sprintf('Vorticity at step %d',step) )
+axis equal tight
+set(gca,'XTick',[],'YTick',[])
+
+%%% PLOTMODES
+% Plot speed and vorticity modes in columns
 
 function plotModes( nPlotted, Spectrum, Amps, Modes )
 
-% visualize only modes with positive angles -- to avoid replication
-  sel = angle(Spectrum) >= 0;
-  Spectrum = Spectrum(sel);
-  Amps = Amps(sel);
-  Modes = Modes(:,:,:,sel);
+%%
+% Visualize only modes with positive angles -- to avoid replication between
+% conjugate pairs of modes
 
-  SpeedModes = squeeze(Modes(1,:,:,:));
-  VortModes = squeeze(Modes(2,:,:,:));
+sel = angle(Spectrum) >= 0;
+Spectrum = Spectrum(sel);
+Amps = Amps(sel);
+Modes = Modes(:,:,:,sel);
 
-  nPlotted = 4;
-  for k = 1:nPlotted
-    subplot(nPlotted, 2, 2*k-1);
-    pcolor( SpeedModes(:,:,k) );
-    shading interp;
-    title({sprintf('%d: Growth=%.1f, Freq=%.1f', k, real(Spectrum(k)),...
-                   imag(Spectrum(k)) ), ...
-           'Speed'});
-    set(gca,'XTick',[],'YTick',[])
-    axis equal tight
+%%
+% Split up modes for speed and vorticity
+SpeedModes = squeeze(Modes(1,:,:,:));
+VortModes = squeeze(Modes(2,:,:,:));
 
-    subplot(nPlotted, 2, 2*k);
-    pcolor( VortModes(:,:,k) );
-    shading interp;
-    title({sprintf('log_{10}(Amplitude)=%.1f, Phase=%.1f \\pi', log10(abs(Amps(k))),...
-                   angle(Amps(k))/k ), ...
-           'Vorticity'});
-    set(gca,'XTick',[],'YTick',[])
-    axis equal tight
-  end
+for k = 1:nPlotted
 
+  %%
+  % Plot Speed mode
+  subplot(nPlotted, 2, 2*k-1);
+  pcolor( SpeedModes(:,:,k) ); shading interp;
+  title({sprintf('%d: Growth=%.1f, Freq=%.1f', k, real(Spectrum(k)),...
+                 imag(Spectrum(k)) ), ...
+         'Speed'});
+  set(gca,'XTick',[],'YTick',[])
+  axis equal tight
+
+  %%
+  % Plot Vorticity mode
+  subplot(nPlotted, 2, 2*k);
+  pcolor( VortModes(:,:,k) ); shading interp;
+
+  title({sprintf('log_{10}(Amplitude)=%.1f, Phase=%.1f \\pi', ...
+                 log10(abs(Amps(k))),...
+                 angle(Amps(k))/k ), ...
+         'Vorticity'});
+  set(gca,'XTick',[],'YTick',[])
+  axis equal tight
 end
