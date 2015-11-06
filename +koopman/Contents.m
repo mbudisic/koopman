aa2590d326f48054f3304c8d67d017316028905d
Marko Budisic <mbudisic@gmail.com>
2015-11-06 15:23:00 -0600

    Updated contents

--- +koopman/Contents.m
+++ +koopman/Contents.m
@@ -3,7 +3,7 @@
 % Files
 %   DMD                     - Compute Koopman modes by "exact" Dynamic Mode Decomposition of Tu et al.
 %   DMD_Duke                - Compute Koopman modes by Dynamic Mode Decomposition algorithm by Duke et al.
-%   DMD_Snapshot            - DMD Compute Koopman modes by "exact" Dynamic Mode Decomposition of Tu et al.
+%   DMD_Snapshot            - Compute Koopman modes by stabilized snapshots algorithm by Chen et al.
 %   KDFT                    - Compute Koopman modes using Discrete Fourier Transform (FFT).
 %   removemean              - Removes time mean from snapshots.
 %   L2optimalModeAmplitudes - Compute optimal L2 reconstruction of data by modes.
